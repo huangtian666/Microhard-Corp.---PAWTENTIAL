64aa80fa7e3ae6ee9d3f79b9b168ebf22c877ed8
Tian Huang <tianhuang@Tians-MBP.ctc>
2024-06-14 13:51:22 +0800

    Delete all files

--- app/screens/Authentication/index.tsx
+++
@@ -1,6 +0,0 @@
-export {default as SignInScreen} from './SignInScreen';
-export {default as SignUpScreen} from './SignUpScreen';
-export {default as ConfirmEmailScreen} from './ConfirmEmail';
-export {default as ResetPasswordScreen1} from './ResetPasswordScreen1';
-export {default as ResetPasswordScreen2} from './ResetPasswordScreen2';
-export {default as ResetPasswordScreen3} from './ResetPasswordScreen3';
--- components/SocialSignInButtons/index.tsx
+++
@@ -1,3 +0,0 @@
-export {default} from './SocialSignInButtons';
-//indexfile-> dun need to write the file name, 
-//just import the file, and it will point to the index file
