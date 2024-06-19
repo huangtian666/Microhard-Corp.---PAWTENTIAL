3a29e30475a20ef79060caee69a47b15d368853e
Tian Huang <tianhuang@Tians-MBP.ctc>
2024-06-19 18:39:20 +0800

    update screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,20 @@ import { Stack } from 'expo-router';
 
 const RootLayout = () => {
   return (
-      <Stack screenOptions={{ headerShown: false }} >
-        <Stack.Screen name="screens/Authentication/SignInScreen" />
-        <Stack.Screen name="screens/Authentication/SignUpScreen" />
-        <Stack.Screen name="screens/Authentication/ConfirmEmail" />
-        <Stack.Screen name="screens/Authentication/ResetPasswordScreen1" />
-        <Stack.Screen name="screens/Authentication/ResetPasswordScreen2" />
-        <Stack.Screen name="screens/Authentication/ResetPasswordScreen3" />
-        <Stack.Screen name='screens/home'/>
-      </Stack>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="screens/Authentication/SignInScreen" />
+      <Stack.Screen name="screens/Authentication/SignUpScreen" />
+      <Stack.Screen name="screens/Authentication/ConfirmEmailScreen" />
+      <Stack.Screen name="screens/Authentication/ResetPasswordScreen1" />
+      <Stack.Screen name="screens/Authentication/ResetPasswordScreen2" />
+      <Stack.Screen name="screens/Authentication/ResetPasswordScreen3" />
+      <Stack.Screen name="screens/WelcomeScreen/WelcomeScreen" />
+      <Stack.Screen name="screens/BoardingQuestions/Question1" />
+      <Stack.Screen name="screens/BoardingQuestions/Question2" />
+      <Stack.Screen name="screens/Home/HomeScreen" />
+    </Stack>
   );
-}
+};
 
 export default RootLayout;
---
+++ app/screens/ToDoList/ToDoListScreen.tsx
@@ -0,0 +1,31 @@
+import {SafeAreaView, Text, Image, StyleSheet, ScrollView, Dimensions, Alert} from 'react-native';
+import React, {useState} from 'react';
+
+ 
+const ToDoList = () => {
+   
+    return (
+        <ScrollView contentContainerStyle={{paddingBottom: 20, backgroundColor: '#FFF2CD'}} >
+            <SafeAreaView style={styles.container}>
+                <Text style ={styles.text1}>To-Do List</Text>   
+            </SafeAreaView>
+        </ScrollView>
+    ); 
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        width: Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
+    },
+    text1: {
+        fontSize: 30,
+        color: '#551B26',
+        fontWeight: 'bold',
+        marginBottom: '10%',
+        marginTop: '15%',
+    },
+});
+
+export default ToDoList;
