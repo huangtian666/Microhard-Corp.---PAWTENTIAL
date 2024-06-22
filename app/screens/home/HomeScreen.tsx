854d603c33b296bf2f0f440e5f71fe2674ade930
Tian Huang <tianhuang@Tians-MBP.ctc>
2024-06-23 01:45:09 +0800

    updated home screen

--- app/screens/home/HomeScreen.tsx
+++ app/screens/home/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TouchableOpacity, Text, View, ScrollView, Dimensions, SafeAreaView} from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View, ScrollView, Dimensions, SafeAreaView } from 'react-native';
 import { router } from 'expo-router';
 import React from 'react';
 import { useTaskContext } from '../../Context/TaskProvider';
@@ -10,30 +10,32 @@ const Home: React.FC = () => {
   const progress = todayTotalTasks > 0 ? todayCompletedTasks / todayTotalTasks : 0;
 
   const viewTask = () => {
-    // Navigation or any function to view tasks
+    router.push('/screens/ToDoList')
   }
 
   return (
-    <ScrollView contentContainerStyle={{ backgroundColor: 'white', flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <ScrollView contentContainerStyle={{ backgroundColor: 'white', flexGrow: 1 }}>
       <SafeAreaView style={styles.container}>
         <View style={styles.card}>
           <View style={styles.taskStatusContainer}>
-            <Text style={styles.title}>Your today's task almost done!</Text>
-            <Progress.Circle
+            <View style={styles.textContainer}>
+              <Text style={styles.title}>Your today's task{'\n'}almost done!</Text>
+              <TouchableOpacity style={styles.button} onPress={viewTask}>
+                <Text style={styles.buttonText}>View Task</Text>
+              </TouchableOpacity>
+            </View>
+            <Progress.Circle style={styles.progressCircle}
               progress={progress}
               size={100}
               borderWidth={8}
-              color="#3399FF"
+              color="white"
               shadowColor="#999"
               bgColor="#fff"
               thickness={8}
             >
-              <Text style={{ fontSize: 18 }}>{Math.round(progress * 100)}%</Text>
+              <Text  style={styles.progressText}>{Math.round(progress * 100)}%</Text>
             </Progress.Circle>
           </View>
-          <TouchableOpacity style={styles.button} onPress={viewTask}>
-            <Text style={styles.buttonText}>View Task</Text>
-          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </ScrollView>
@@ -43,15 +45,15 @@ const Home: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 20, // Move the card closer to the top
   },
   card: {
     backgroundColor: '#5C50E6', // Background color of the card
     borderRadius: 20, // Rounded corners
-    width: Dimensions.get('window').width*0.9,
-    height: Dimensions.get('window').height*0.2,
-    padding:20,
+    width: Dimensions.get('window').width * 0.9,
+    height: Dimensions.get('window').height * 0.175,
+    padding: 20,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -66,14 +68,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between', // Space between elements
   },
+  textContainer: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'flex-start', // Align text and button vertically
+    paddingLeft: 20,
+  },
   title: {
     fontSize: 18,
+    fontWeight: 'bold',
     color: '#fff',
-    flex: 1, // Allow text to take up available space
+    textAlign: 'left',
+    marginBottom: 10,
   },
   button: {
+    marginTop: 5,
     padding: 10,
-    borderRadius: 5,
+    borderRadius: 8,
     backgroundColor: '#7F67F6',
     alignSelf: 'flex-start', // Align button to the start of the container
   },
@@ -81,6 +93,16 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
   },
+  progressCircle: {
+    marginRight: '5%',
+  },
+  progressText: {
+    marginTop: 10,
+    fontSize: 18,
+    color: '#fff', // Set text color to white
+    fontWeight: 'bold',
+    textAlign: 'center', // Center the text
+  }
 });
 
 export default Home;
